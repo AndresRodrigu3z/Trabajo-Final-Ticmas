64e405479c0da60ad3a325bb29cc578ca024171d
AndresRodrigu3z <122901809+AndresRodrigu3z@users.noreply.github.com>
2023-03-09 15:53:59 -0300

    Add files via upload

---
+++ Proyecto CV Ticmas/java script/eventos.js
@@ -0,0 +1,35 @@
+function enAmarillo1() {
+Element =document.getElementById("pers");
+Element.src="img/personal2.png";
+}
+function enNegro1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal.png";
+}
+
+function enAmarillo2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral2.png";
+}
+ function enNegro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.png";
+}
+
+function enAmarillo3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades2.png";
+}
+function enNegro3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.png";
+}
+
+function enAmarillo4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios2.png";
+}
+function enNegro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.png";
+}
